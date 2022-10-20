2d76432feb845085b610e67ed35863a1d36d3592
azizovxikmatjon04 <azizovxikmatjon04@gmail.com>
2022-10-20 19:15:59 +0500

    bubble sort

--- sorts.cpp
+++ sorts.cpp
@@ -1,7 +1,33 @@
 ﻿#include<iostream>
+#include<array>
+#include<vector>
 using namespace std;
-int main()
-{
-	cout << "Hello World";
-	cout << "Not hello world";
+
+int ARRAY[] = { 1, 7, 2, 78, 4, 3, 5, 7, 4, 5 };
+
+
+void show(int *arr, int length) {
+	for (int i = 0; i < length; i++) {
+		cout << arr[i] << " ";
+	}
+}
+
+void bubbleSort() {
+	for (int i = 0; i < size(ARRAY); i++) {
+		for (int j = 0; j < size(ARRAY) - 1 - i; j++) {
+			if (ARRAY[j] > ARRAY[j + 1]) {
+				int buff = ARRAY[j];
+				ARRAY[j] = ARRAY[j + 1];
+				ARRAY[j + 1] = buff;
+			}
+		}
+	}
+
+	show(ARRAY, size(ARRAY));
+}
+
+
+int main() {
+	bubbleSort();
+	return 0;
 }
